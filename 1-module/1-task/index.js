9f42376a878fb543468093eeaf0142dde87b8b81
Andrey Khayustov <textilehappiness@gmail.com>
2023-12-16 17:46:41 +0200

    написал функцию factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  let i = 1;
+  while (i < n) {
+    result *= (n - i);
+    i++;
+  }
+  if (n === 0 || n === 1) {
+  return 1;
+  }
+  return result;
 }
