b87c535f6a97fdfe775e3c5e54f39fe4731eddb9
Andrey Khayustov <textilehappiness@gmail.com>
2023-12-18 12:01:19 +0200

    функция проверки на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowString = str.toLowerCase();
+  let spam1 = '1xBet'.toLowerCase();
+  let spam2 = 'XXX'.toLowerCase();
+  
+   if (lowString.includes(spam1) || lowString.includes(spam2)) {
+    
+    return true;
+    
+    } else return false;
 }
