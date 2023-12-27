34f944535ea9c39f7d1847a37ca7bd59c7b9dba0
Andrey Khayustov <textilehappiness@gmail.com>
2023-12-27 21:31:17 +0200

    функция преобразования строки в camelcase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,21 @@
 function camelize(str) {
-  // ваш код...
+   // порезать строку на части
+
+ let arr = str.split('-');
+
+  // преобразование элементов
+    
+  let result = arr.map((item, index, arr) => { 
+    
+   if (index === 0) {
+    
+     return arr[0];
+    
+  }  else return arr[index][0].toUpperCase() + arr[index].slice(1);
+        
+  }); 
+  
+ // собрать все в строку 
+   
+  return result.join('');
 }
