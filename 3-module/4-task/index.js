63cb98759f556a961701b4dfb462177ffd742aa5
Andrey Khayustov <textilehappiness@gmail.com>
2023-12-29 13:39:20 +0200

    функция показывает имена и зарплаты

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,27 @@
 function showSalary(users, age) {
-  // ваш код...
+  
+// фильтрация по возрасту
+
+let filtAge = users.filter((user) => {return (user.age <= age)});
+
+// выбрать то, что нужно и добавить перенос строки
+  let result = [];
+
+  for (let item of filtAge) {
+
+    if (item == filtAge.at(-1))  { 
+
+     result.push(`${item.name}, ${item.balance}`);
+   }
+
+    else {
+
+     result.push(`${item.name}, ${item.balance}\n`);
+
+   }
+ };
+ 
+// собрать в строку
+  return result.join('');
+
 }
