1a126b80717820a232f57e9e99f81cbbb242f531
Andrey Khayustov <textilehappiness@gmail.com>
2024-01-01 15:22:23 +0200

    функция создает список из массива

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  let friendsList = document.createElement('UL');
+
+  // добавить в body 'ul'
+  document.body.append(friendsList);
+  
+  // взять из массива объектов, то что нужно и вставить в 'ul', обвернув в 'li'
+  friends.forEach( (friend) => {
+  
+    friendsList.insertAdjacentHTML('beforeEnd',`<li>${friend.firstName} ${friend.lastName}</li>`);
+  
+   });
+  
+    return friendsList;
 }
