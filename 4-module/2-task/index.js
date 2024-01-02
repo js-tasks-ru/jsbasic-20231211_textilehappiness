cd86f0661bb41173fbf7c82dc94873db9f2e3a2f
Andrey Khayustov <textilehappiness@gmail.com>
2024-01-02 14:30:31 +0200

    функция выделяет ячейки красным цветом

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+
+// получить доступ к ячейке с помощью цикла и добавить свойство
+  for (let i = 0; i < table.rows.length; i++) {
+      
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+      
+  }
 }
